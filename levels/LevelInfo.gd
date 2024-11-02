40d57d900bc761aa4d65584d5762e2bde4b65134
Timothy Stiles <Patashu@hotmail.com>
2024-11-02 19:20:10 +1100

    dummy tileset and level

--- levels/LevelInfo.gd
+++ levels/LevelInfo.gd
@@ -4,7 +4,8 @@ class_name LevelInfo
 export var level_name = "";
 export var level_author = "Patashu";
 export var level_replay = "";
-export var wiring = "";
 var map_x_max = -1;
 var map_y_max = -1;
+export var target_sky : Color = Color("25272A");
 var layers = -1;
+export var target_track = -1;
